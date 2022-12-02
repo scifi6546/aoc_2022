fa0b96f2c9b9ecaf9faf496ce974368bb139e7b0
Nick <nickalexeev@protonmail.com>
2022-12-01 20:53:45 -0900

    2p1 done

--- advent/lib/ac.ex
+++ advent/lib/ac.ex
@@ -36,16 +36,80 @@ defmodule AC do
       |> Enum.take(3)
       |> Enum.sum()
   end
+  def two_test_input() do
+"""
+A Y
+B X
+C Z
+"""
+  end
+  def two_p1(input) do
+    String.split(input,"\n")
+    |> Enum.filter(fn x -> String.length(x)!=0 end)
+    |> Enum.map(fn line -> String.split(line) end)
+    |> Enum.map(fn pair -> AC.TwoHelper.parse_line(pair) end)
+    |> Enum.map(fn play -> AC.TwoHelper.process_play(play) end)
+    |> Enum.sum()
+  end
+  def two_p1() do
+    two_p1(two_test_input())
+  end
+end
+defmodule AC.TwoHelper do
+  def parse_line(line) do
+    %{opponent: hd(line), self: hd(tl(line))}
+  end
+  @doc """
+  gets score for given play
+  iex> process_play(%{opponent: "A", self: "Y"})
+  8
+  iex> process_play(%{opponent: "B", self: "X"})
+  1
+  iex> process_play(%{opponent: "C", self: "Z"})
+  6
+  """
+  def process_play(play) do
+    cond do
+      play[:opponent] == "A" -> opp_a(play)
+      play[:opponent] == "B" -> opp_b(play)
+      play[:opponent] == "C" -> opp_c(play)
+      true -> raise "invalid opponent move"
+    end
+  end
+  @doc """
+  Gets score if opponent played a
+  """
+  defp opp_a(play) do
+    cond do
+      play[:self] == "X" -> 1 + 3
+      play[:self] == "Y" -> 2 + 6
+      play[:self] == "Z" -> 3 + 0
+      true -> raise "invalid player move"
+    end
+  end
+  defp opp_b(play) do
+    cond do
+      play[:self] == "X" -> 1 + 0
+      play[:self] == "Y" -> 2 + 3
+      play[:self] == "Z" -> 3 + 6
+      true -> raise "invalid player move"
+    end
+  end
+  defp opp_c(play) do
+    cond do
+      play[:self] == "X" -> 1 + 6
+      play[:self] == "Y" -> 2 + 0
+      play[:self] == "Z" -> 3 + 3
+      true -> raise "invalid player move"
+    end
+  end
 end
 defmodule AC.OneHelper do
   def parse_file(path) do
-    load_file(path)
+    GeneralHelpers.load_file(path)
     |> parse_to_struct()
   end
-  def load_file(path) do
-    {:ok,file_content} = File.read(path)
-    file_content
-  end
+
   def parse_to_struct(str) do
     String.split(str, "\n")
     |> Enum.chunk_by(fn(x) -> x=="" end)
@@ -57,7 +121,7 @@ defmodule AC.OneHelper do
   end
   def parse_int(s) do
     {num,_} = Integer.parse(s)
-        num
+    num
   end
   def parse_int_list(l) do
     Enum.map(l,fn x ->
@@ -69,5 +133,11 @@ defmodule AC.OneHelper do
   end
   defmodule FoodSum do
     defstruct sum: 0, elements: []
+  end
+end
+defmodule GeneralHelpers do
+  def load_file(path) do
+    {:ok,file_content} = File.read(path)
+    file_content
   end
 end
--- advent/test/ac_test.exs
+++ advent/test/ac_test.exs
@@ -4,8 +4,16 @@ defmodule ACTest do
   test "advent 1" do
     assert AC.one("./test/test_input/1") == 24000
   end
-  test "advent 2" do
+  test "advent 1p2" do
     assert AC.one_p2("./test/test_input/1") == 45000
   end
-
+  test "advent 2p1" do
+    input =
+"""
+A Y
+B X
+C Z
+"""
+    assert AC.two_p1(input) == 15
+  end
 end
