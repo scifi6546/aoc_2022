b52c6d0d00ec9959afdf83f3029a38c4264bd741
Nick <nickalexeev@protonmail.com>
2022-12-09 14:39:37 -0900

    clean up

--- advent/lib/AC/eight.ex
+++ advent/lib/AC/eight.ex
@@ -31,6 +31,12 @@ defmodule AC.Eight do
       end)
       |> Enum.reduce(fn x, acc -> acc <> "\n" <> x end)
     end
+
+    def reduce(grid, acc, fun) do
+      Tuple.to_list(grid)
+      |> Enum.flat_map(fn row -> Tuple.to_list(row) end)
+      |> Enum.reduce(acc, fn x, acc -> fun.(x, acc) end)
+    end
   end
 
   @spec test_input() :: String
@@ -48,37 +54,39 @@ defmodule AC.Eight do
     21
   end
 
+  @spec test_output_part2 :: 8
   def test_output_part2 do
     8
   end
 
+  @spec problem1(String.t()) :: String.t()
   def problem1(input) do
-    grid =
-      String.split(input, "\n")
-      |> Enum.filter(fn s -> String.length(s) != 0 end)
-      |> Enum.map(fn s -> String.graphemes(s) end)
-      |> Enum.map(fn row -> list_to_ints(row) end)
-      |> Enum.map(fn row -> List.to_tuple(row) end)
-      |> List.to_tuple()
+    grid = grid_from_string(input)
 
-    Enum.zip([
-      get_u_d_vis_grid(grid),
-      get_d_u_vis_grid(grid),
-      get_l_r_vis_grid(grid),
-      get_r_l_vis_grid(grid)
-    ])
-    |> Enum.map(fn rows -> compare_rows(rows) end)
-    |> Enum.map(fn row ->
-      Enum.map(row, fn item ->
-        if item == :visible do
-          1
-        else
-          0
-        end
-      end)
+    Grid.zip(
+      [
+        get_u_d_vis_grid(grid),
+        get_d_u_vis_grid(grid),
+        get_l_r_vis_grid(grid),
+        get_r_l_vis_grid(grid)
+      ],
+      fn x ->
+        Tuple.to_list(x)
+        |> Enum.reduce(:hidden, fn x, acc ->
+          cond do
+            x == :visible -> :visible
+            x == :hidden -> acc
+            true -> raise "state error"
+          end
+        end)
+      end
+    )
+    |> Grid.reduce(0, fn x, acc ->
+      cond do
+        x == :hidden -> acc
+        x == :visible -> acc + 1
+      end
     end)
-    |> Enum.map(fn row -> Enum.sum(row) end)
-    |> Enum.sum()
   end
 
   def problem2(input) do
@@ -99,8 +107,6 @@ defmodule AC.Eight do
 
     Grid.zip([l_r, r_l, u_d, d_u], fn x_tuple -> Tuple.to_list(x_tuple) |> Enum.product() end)
     |> Grid.max()
-
-    # {l_r, r_l, u_d, d_u}
   end
 
   def grid_from_string(input) do
@@ -157,15 +163,17 @@ defmodule AC.Eight do
       Enum.map(AC.range(0, i), fn j -> %{i: i, j: j, elem: elem(row, j)} end)
       |> Enum.reverse()
       |> Enum.reduce(%{largest: elem(row, i), current_distance: i, in_shadow: false}, fn x, acc ->
+        current_distance = i - x[:j]
+
         cond do
           acc[:in_shadow] == false && x[:elem] >= acc[:largest] ->
             Map.put(acc, :largest, x[:elem])
-            |> Map.put(:current_distance, i - x[:j])
+            |> Map.put(:current_distance, current_distance)
             |> Map.put(:in_shadow, true)
 
           acc[:in_shadow] == true && x[:elem] > acc[:largest] ->
             Map.put(acc, :largest, x[:elem])
-            |> Map.put(:current_distance, i - x[:j])
+            |> Map.put(:current_distance, current_distance)
 
           true ->
             acc
@@ -203,16 +211,6 @@ defmodule AC.Eight do
     # |> Enum.map(fn grid_row -> Tuple.to_list(grid_row) end)
   end
 
-  defp is_visible(atom) do
-    IO.inspect(atom)
-
-    if atom == :visible do
-      true
-    else
-      false
-    end
-  end
-
   def get_u_d_vis_grid(grid) do
     new_grid =
       Enum.map(0..(get_x_size(grid) - 1), fn x -> get_grid_col(grid, x) end)
@@ -221,6 +219,7 @@ defmodule AC.Eight do
 
     # flipping grid back
     Enum.map(0..(get_x_size(grid) - 1), fn x -> get_grid_col(new_grid, x) end)
+    |> List.to_tuple()
   end
 
   def get_d_u_vis_grid(grid) do
@@ -231,16 +230,19 @@ defmodule AC.Eight do
 
     # flipping grid back
     Enum.map(0..(get_x_size(grid) - 1), fn x -> get_grid_col(new_grid, x) end)
+    |> List.to_tuple()
   end
 
   def get_l_r_vis_grid(grid) do
     Enum.map(0..(get_y_size(grid) - 1), fn y -> get_grid_row(grid, y) end)
     |> Enum.map(fn row -> row_visible_l_to_r(row) end)
+    |> List.to_tuple()
   end
 
   def get_r_l_vis_grid(grid) do
     Enum.map(0..(get_y_size(grid) - 1), fn y -> get_grid_row(grid, y) end)
     |> Enum.map(fn row -> row_visible_r_to_l(row) end)
+    |> List.to_tuple()
   end
 
   @doc """
