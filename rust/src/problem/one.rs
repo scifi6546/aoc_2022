79904b7c2169a8ad88175a3620441dcccdd87489
Nicholas Alexeev <nickalexeev@protonmail.com>
2023-11-25 10:51:09 -0900

    done with one

--- rust/src/problem/one.rs
+++ rust/src/problem/one.rs
@@ -1,31 +1,50 @@
 use super::Problem;
 use std::cmp::max;
+
 pub const ONE: Problem = Problem {
-    number: 0,
+    number: 1,
     problem_a: a,
     problem_b: b,
 };
-fn a(input: &str) -> String {
-    let elves = input
+#[derive(Clone, Copy)]
+struct Elf {
+    calories: u32,
+}
+fn parse_input(input: &str) -> Vec<Elf> {
+    input
         .split("\n\n")
-        .map(|elf| {
-            elf.split_whitespace()
+        .map(|elf| Elf {
+            calories: elf
+                .split_whitespace()
                 .map(|food| food.parse::<u32>().expect("failed to parse"))
-                .sum::<u32>()
+                .sum::<u32>(),
         })
-        .fold(0u32, |acc, x| max(acc, x));
+        .collect()
+}
+fn a(input: &str) -> String {
+    let elves = parse_input(input)
+        .iter()
+        .fold(0u32, |acc, x| max(acc, x.calories));
     format!("{}", elves)
 }
 fn b(input: &str) -> String {
-    todo!()
+    let mut top_elves = [
+        Elf { calories: 0 },
+        Elf { calories: 0 },
+        Elf { calories: 0 },
+    ];
+    let mut elves = parse_input(input);
+    elves.sort_by(|a, b| a.calories.cmp(&b.calories));
+    let sum = elves[elves.len() - 1].calories
+        + elves[elves.len() - 2].calories
+        + elves[elves.len() - 3].calories;
+    sum.to_string()
 }
 
 #[cfg(test)]
 mod test {
     use super::*;
-    #[test]
-    fn one() {
-        let input = r#"
+    const TEST_INPUT: &'static str = r#"
 1000
 2000
 3000
@@ -41,7 +60,14 @@ mod test {
 
 10000
     "#;
-        let r = a(input);
+    #[test]
+    fn one() {
+        let r = a(TEST_INPUT);
         assert_eq!(&r, "24000")
+    }
+    #[test]
+    fn test_b() {
+        let r = b(TEST_INPUT);
+        assert_eq!(&r, "45000");
     }
 }
